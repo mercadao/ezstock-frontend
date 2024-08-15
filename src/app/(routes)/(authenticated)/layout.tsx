3e9d254675fe8e8771c4b31b8c51cc8bcdd7e751
Elvis Claudino <102040112+elvisclaudino@users.noreply.github.com>
2024-08-14 21:34:44 -0300

    fix: layout height

--- src/app/(routes)/(authenticated)/layout.tsx
+++ src/app/(routes)/(authenticated)/layout.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import React, { useState } from 'react';
-import SideBar from '@/app/components/organisms/SideBar';
+import React, { useState } from "react";
+import SideBar from "@/app/components/organisms/SideBar";
 
 interface LayoutProps {
   children: React.ReactNode;
@@ -16,14 +16,22 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
 
   return (
     <div className="flex h-full">
-      <div className={`fixed top-0 left-0 h-full ${isSidebarClosed ? 'w-[75px]' : 'w-sidebar-width'} transition-all duration-300`}>
+      <div
+        className={`fixed top-0 left-0 h-full ${
+          isSidebarClosed ? "w-[75px]" : "w-sidebar-width"
+        } transition-all duration-300`}
+      >
         <SideBar onToggle={handleToggleSidebar} />
       </div>
-      <main className={`flex-grow ${isSidebarClosed ? 'ml-[100px]' : 'ml-sidebar-width'} bg-offwhite h-full min-h-screen transition-all duration-300`}>
+      <main
+        className={`flex-grow ${
+          isSidebarClosed ? "ml-[100px]" : "ml-sidebar-width"
+        } bg-offwhite min-h-screen transition-all duration-300`}
+      >
         {children}
       </main>
     </div>
   );
-}
+};
 
 export default Layout;
