4de6ab914322982dbb0ee74bd0349cd7382ea27c
Gabriel Fasolim <gabrielfasolim@hotmail.com>
2024-09-02 19:14:13 -0300

    [add] merge zip gb

--- src/app/components/atoms/Divider/index.tsx
+++
@@ -1,9 +0,0 @@
-interface DividerProps {
-    color?: string;
-}
-
-export default function Divider({color = "primary-400"}:DividerProps){
-    return(
-        <div className={`w-full h-[1px] bg-${color}`} />
-    )
-}
